7c0e497d36eaa96083f52f2776e8e031de6f7309
nic0im <nicow054@gmail.com>
2026-06-02 10:20:12 -0400

    Added findBlock util, for tunneler functionality

---
+++ src/main/java/com/nic0im/minebot/Utils/BlockScannerUtils.java
@@ -0,0 +1,37 @@
+package com.nic0im.minebot.Utils;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class BlockScannerUtils {
+
+    public static BlockPos findBlock(
+            Level level,
+            BlockPos center,
+            Direction forward) {
+
+        Direction right = forward.getClockWise();
+
+        for (int distance = 1; distance <= 2; distance++) {
+            for (int y = 1; y <= 3; y++) {
+                for (int width = -1; width <= 1; width++) {
+
+                    BlockPos pos = center
+                            .relative(forward, distance)
+                            .relative(right, width)
+                            .above(y);
+
+                    if (!level.getBlockState(pos).isAir()) {
+                        return pos;
+                    }
+                }
+            }
+        }
+
+        return null;
+    }
+
+
+}
--- src/main/java/com/nic0im/minebot/handlers/MineBotActions.java
+++ src/main/java/com/nic0im/minebot/handlers/MineBotActions.java
@@ -1,13 +1,21 @@
 package com.nic0im.minebot.handlers;
 
+import net.minecraft.core.Direction;
+
 public class MineBotActions {
 
 
     public static void handleLooking(){
 
+
+
     }
 
     public static void handleBreaking(){
+
+    }
+
+    public static void handleWalking(){
 
     }
 
